afacc7b1176a4751f3dc17321fd0244800db4798
David Perez <david.pd2690@gmail.com>
2021-11-23 13:02:01 -0600

    Updates

---
+++ precache-manifest.3c2931b3bb4eb3fe8846269680921867.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b94d35383284953ebd8e7a4395f9c1",
+    "url": "/keep-app/index.html"
+  },
+  {
+    "revision": "06fa7985f168e0b884e3",
+    "url": "/keep-app/static/js/2.120090ea.chunk.js"
+  },
+  {
+    "revision": "ae9d1b8bdbdeb295cf6f",
+    "url": "/keep-app/static/js/main.3596d9ec.chunk.js"
+  },
+  {
+    "revision": "c424166994d5f790f009",
+    "url": "/keep-app/static/js/runtime-main.8cad2315.js"
+  }
+]);
